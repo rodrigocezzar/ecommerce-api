f8be1852c2ba9af847b61da6f20185b48a6f291f
rodrigocezzar <rodrigocezzar85@gmail.com>
2022-10-15 21:17:49 -0300

    create model product_categories and validations

---
+++ spec/models/product_category_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ProductCategory, type: :model do
+  it { is_expected.to belong_to :product }
+  it { is_expected.to belong_to :category }
+end
