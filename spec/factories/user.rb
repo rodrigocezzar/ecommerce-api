1f7820c28426210fe970a6c2932a798c453b2714
rodrigocezzar <rodrigocezzar85@gmail.com>
2022-10-16 22:44:09 -0300

    setup tests request

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,10 +2,10 @@
 
 FactoryBot.define do
   factory :user do
-    name { Fake::Name.name }
+    name { Faker::Name.name }
     email { Faker::Internet.email }
     password { '123456' }
-    passwor_confirmation { '123456' }
+    password_confirmation { '123456' }
     profile { %i[admin client].sample }
   end
 end
