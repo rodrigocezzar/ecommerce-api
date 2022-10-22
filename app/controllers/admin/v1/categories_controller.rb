83986acdbd1d7e939ff226b99f5b0d2f818b83bd
rodrigocezzar <rodrigocezzar85@gmail.com>
2022-10-22 14:18:53 -0300

    improved category creation

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -9,6 +9,28 @@ module Admin
 
         render json: @categories, each_serializer: CategoriesSerializer
       end
+
+      def create
+        @category = Category.new
+        @category.attributes = category_params
+
+        save_category!
+      end
+
+      private
+
+      def category_params
+        return {} unless params.key?(:category)
+
+        params.require(:category).permit(:id, :name)
+      end
+
+      def save_category!
+        @category.save!
+        render json: @category, serializer: CategoriesSerializer
+      rescue
+        render_error(fields: @category.errors.messages)
+      end
     end
   end
 end
