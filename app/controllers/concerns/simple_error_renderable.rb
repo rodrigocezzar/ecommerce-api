9aec3a8466977d4e082d18faf3b4ddc5fae86c9f
Rodrigo de Abreu Cezar <rodrigodeabreucezar@MacBook-Air-de-Rodrigo.local>
2023-01-21 22:07:22 -0300

    refatoring error rendering

---
+++ app/controllers/concerns/simple_error_renderable.rb
@@ -0,0 +1,14 @@
+module SimpleErrorRenderable
+  extend ActiveSupport::Concern
+
+  # included do
+  #   render_error
+  # end
+
+  def render_error(message: nil, fields: nil, status: :unprocessable_entity)
+    errors = {}
+    errors['fields'] = fields if fields.present?
+    errors['message'] = message if message.present?
+    render json: { errors: errors }, status: status
+  end
+end
